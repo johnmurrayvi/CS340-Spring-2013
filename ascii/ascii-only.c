81686402c16c029ba9f5c0c227443f95416b9985
John Murray <johnmurrayvi@gmail.com>
2013-02-09 01:58:24 -0500

    Add some more error messages, remove the pointer cruft from the fopen line, print errno after perror

--- ascii/ascii-only.c
+++ ascii/ascii-only.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <errno.h>
 
 int main(int argc, char **argv)
 {
@@ -11,10 +12,11 @@ int main(int argc, char **argv)
 		exit(1);
 	}
 	
-	f = fopen(*++argv,"r");
+	f = fopen(argv[1],"r");
 
     if (!f) {
-        perror("error from fopen:");
+        perror("error from fopen");
+        errno;
         return 1;
     }
 	
@@ -22,8 +24,13 @@ int main(int argc, char **argv)
         c = (unsigned char) fgetc(f);
         if (c > 0 && c < 128)
             printf("%c", (int) c);
+        else if (c != 255) {        /* EOF = -1  -->  255 for unsigned char */
+            fprintf(stderr, "Non-ascii character detected\n");
+            return(1);
+        }
     }
 
     fclose(f);
     return 0;
 }
+
