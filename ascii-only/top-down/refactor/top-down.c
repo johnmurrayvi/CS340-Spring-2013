5c31bc5a316440cc550996f31dfbb698bb989eb8
John Murray <johnmurrayvi@gmail.com>
2013-03-20 11:02:16 -0400

    clean up comments a bit

--- ascii-only/top-down/refactor/top-down.c
+++ ascii-only/top-down/refactor/top-down.c
@@ -1,27 +1,15 @@
-/*#include <stdio.h>
-#include <string.h>
-#include <unistd.h>
-#include <time.h>
-*/
+
 #include "top-down.h"
-/*typedef struct F {
-    char *Infile;
-    char *Outfile;
-    char *Logfile;
-    FILE *In;
-    FILE *Out;
-    FILE *Log;
-} iofiles;
-*/
+
 void show_usage()
 {
     printf("\nUsage: "
-                 "ascii-only [-r replacement] [-l log_file ] [-i input_file] [-o output_file]\n\n"
-                 "  Missing input/output files default to stdin/stdout.\n"
-                 "  Replacement is one of the following: \n"
-                 "   character\n"
-                 "   hex constant ( i.e., 0x3F )\n"
-                 "   integer from 0 to 255 with decimal point ( i.e., 132. )\n\n");
+           "ascii-only [-r replacement] [-l log_file ] [-i input_file] [-o output_file]\n\n"
+           "  Missing input/output files default to stdin/stdout.\n"
+           "  Replacement is one of the following: \n"
+           "   character\n"
+           "   hex constant ( i.e., 0x3F )\n"
+           "   integer from 0 to 255 with decimal point ( i.e., 132. )\n\n");
 }
 
 /*this function is needed, unless we rewrite the switch statement. Vose wrote it to
@@ -34,89 +22,105 @@ program would always make it through the switch statement and the error would al
 this function back*/
 int determine_replacement(int i, int argc, char **argv)
 {
-  int n, R, res;
+    int n, R, res;
 
-  if (argv[i]){                                    // -r option is followed by a value
-	switch(argv[i][1]){                            // second character of value
-	case 'X':
-	case 'x':                                      // hex replacement
-	  res = sscanf(argv[i],"%x%n", &R,&n);
-	  if ((res !=  1)||                            // argv[i] won't read as hex
-		  (R < 0)||                                // R too small
-		  (R > 255)||                              // R too big
-          (strlen(argv[i]) > 4)||                  // argv[i] too long
-          (argv[i][0] != '0')||                    // argv[i] doesn't begin "0x" or "0X"
-          (argv[i][n]))                            // argv[i] has trailing junk
-        break;
-      return R;                                    // return the replacement if it was read
-	  
-    case  0 :                                      // character replacement
-      R = argv[i][0];
-      return R;                                    // return the replacement if it was read
-	  
-    default :                                      // decimal replacement
-      res = sscanf(argv[i],"%d.%n", &R,&n);
-	  if ((res != 1)||  // argv[i] won't read as decimal
-          (R < 0)||                                // R too small
-          (R > 255)||                              // R too big
-          (strlen(argv[i]) > 4)||                  // argv[i] too long
-          (argv[i][n]))                            // argv[i] has trailing junk
-        break;
-      return R;                                    // return the replacement if it was read
+    /* Make sure there is a value following the -r flag */
+    if (argv[i]) {
+        switch(argv[i][1]) {
+            /* Check if hex was given */
+            case 'X':
+            case 'x':
+                res = sscanf(argv[i],"%x%n", &R,&n);
+                /* 
+                 * Error check the hex read: 
+                 * range of R, bad specification, or
+                 * strlen > 4 --> R > 1 byte,
+                 * trailing bytes, etc. 
+                 * break if it fails any
+                 */
+                if ((res !=  1) || (argv[i][0] != '0') || 
+                    (R < 0) || (R > 255) ||                 
+                    (strlen(argv[i]) > 4) || (argv[i][n]))  
+                    break;                  
+                /* It is hex, so return it. */
+                 return R;
+
+            /* Check if it is character (via trailing '\0') */
+            case  0 :
+                R = argv[i][0];
+                return R;
+
+            /* Check for decimal replacement */
+            default :
+                /* Error check the decimal read break if it fails any */
+                res = sscanf(argv[i],"%d.%n", &R,&n);
+                if ((res != 1) || (strlen(argv[i]) > 4) ||
+                    (R < 0) || (R > 255) || (argv[i][n]))
+                    break;
+                /* Return the decimal replacement*/
+                return R;
+        }
     }
-  }
-  
-  return -1;                                    // error condition
+    /* If we got here, there was a problem so return an error */
+    return -1;
 }
 
 int option(char *s, int argc, char **argv, int *flags)
 {
     int i, j;
 
-    for (i = 0, j = 1; j < argc; j++)      // traverse command line arguments
-        if (!strcmp(s, argv[j])){             // j_th argument matches option s
-            if (i)                             // multiple occurrences of s
-                *flags = argc;                         // error condition
+    /* Traverse arguments and  */
+    for (i = 0, j = 1; j < argc; j++) {
+        if (!strcmp(s, argv[j])) {
+            if (i)                         /* error: multiple occurrences of s */
+                *flags = argc;
             else {
-                i = j+1;                         // option value is argv[*i]
-                *flags += 2;                           // account for two arguments
+                i = j+1;                   /* option value is argv[*i] */
+                *flags += 2;               /* account for flag and its val */
             }
         }
-		
-    return i;                              // zero, or else index of option value
+    }
+    /* return 0 or the index of option value */
+    return i;
 }
 
 int args_ok(int argc, char **argv, int *R, iofiles *f)
 {
     int i, flags;
 
-    flags = 0;			                           // flags is the count of args encountered, or argc if error
+    /* flags is the count of args encountered, or argc if error */
+    flags = 0;
 
-	i = option("-i", argc, argv, &flags);				            // infile (if problem, set C to argc)
+    /* Check if infile file was specified */
+    i = option("-i", argc, argv, &flags);
     if (i != 0)
-        f->Infile = (void *)argv[i];           // filename
+        f->Infile = (void *)argv[i];
 
-	i = option("-o", argc, argv, &flags);				            // outfile (if problem, set C to argc)
+    /* Check if output file was specified */
+    i = option("-o", argc, argv, &flags);
     if (i != 0)
-        f->Outfile = (void *)argv[i];          // filename
+        f->Outfile = (void *)argv[i];
 
-	i = option("-l", argc, argv, &flags);		                    // logfile (if problem, set C to argc)
+    /* Check if log flag was specified */
+    i = option("-l", argc, argv, &flags);
     if (i != 0)
-        f->Logfile = (void *)argv[i];          // filename
+        f->Logfile = (void *)argv[i];
 
-	i = option("-r", argc, argv, &flags);                           // replacement (if problem, set C to argc)
-    if (i != 0)
-	{
-		*R = determine_replacement(i, argc, argv);	            // determine replacement character (-1 indicates error)
+    /* Check if replacement was specified */
+    i = option("-r", argc, argv, &flags);           // replacement (if problem, set flags to argc)
+    if (i != 0) {
+        *R = determine_replacement(i, argc, argv);  // determine replacement character (-1 indicates error)
         if(i == -1)
             flags = argc;
-	}
+    }
 
-    if (1+flags == argc)                                            // no errors and all arguments acounted for
-        return 1;                              // success
+    /* If 1+flags == argc, all args accounted for and no errors have occured */
+    if (1+flags == argc)
+        return 1;
 
+    /* If we get here, something isn't right with the arguemtns */
     show_usage();
-    return 0;                                  // failure
+    return 0;
 }
 
 int io_files_ok(iofiles *f)
@@ -125,37 +129,40 @@ int io_files_ok(iofiles *f)
     
     t = time(NULL);                            // get current time
 
-    if (!(f->Infile))                          // no infile specified
-        f->In = stdin;                             // stdin is default
+    /* Check for input file */
+    if (!(f->Infile))
+        f->In = stdin;
     else {
-        f->In = fopen(f->Infile, "r");         // use infile specified
-        if (f->In == NULL){                        // cant open infile
-            perror("infile");                          // show error
-            return 0;                                  // return failure
+        f->In = fopen(f->Infile, "r");
+        if (f->In == NULL) {
+            perror("infile");
+            return 0;
         }
     }
-    if (!(f->Outfile))                         // no outfile specified
-        f->Out = stdout;                           // stdout is default
+    /* Check for output file */
+    if (!(f->Outfile))
+        f->Out = stdout;
     else {
-        f->Out = fopen(f->Outfile, "w");           // use outfile specified
-        if (f->Out == NULL){                       // cant open outfile
-            perror("outfile");                         // show error
-            return 0;                                  // return failure
+        f->Out = fopen(f->Outfile, "w");
+        if (f->Out == NULL) {
+            perror("outfile");
+            return 0;
         }
     }
-    if (f->Logfile){                           // logfile specified
+    /* Check for logfile */
+    if (f->Logfile) {
         f->Log = fopen(f->Logfile, "w");
-        if (f->Log == NULL){                       // cant open logfile
-            perror("logfile");                         // show error
-            return 0;                                  // return failure
+        if (f->Log == NULL) {
+            perror("logfile");
+            return 0;
         }
-		
-        fprintf(f->Log,"%s\n", f->Infile);         // put infile name in logfile
-        fprintf(f->Log,"%s\n", f->Outfile);        // put outfile name in logfile
-        fprintf(f->Log,"%s\n", ctime(&t));         // put date in logfile
+        
+        fprintf(f->Log,"%s\n", f->Infile);
+        fprintf(f->Log,"%s\n", f->Outfile);
+        fprintf(f->Log,"%s\n", ctime(&t));
         fprintf(f->Log,"Ln [Pos]:  <Val>\n");
     }
-    return 1;                                  // return success
+    return 1;
 }
 
 void initialize_iofiles(iofiles *f)
@@ -180,15 +187,15 @@ int main(int argc, char **argv)
     initialize_iofiles(&f);
 
     aok = args_ok(argc, argv, &R, &f);                 // check syntax
-	iok = io_files_ok(&f);                             // open files
-	if (aok && iok)                                    // if everything is ok
-	{
-		C = getc(f.In);
+    iok = io_files_ok(&f);                             // open files
+    if (aok && iok)                                    // if everything is ok
+    {
+        C = getc(f.In);
         while (C != EOF) {             // C is the character (byte) read
             if (!(128 & C)) {           // C is an ascii character
                 fputc(C, f.Out);  // output C
                 P++;            // keep track of character position for logging
-                if (C == '\n'){ // new line
+                if (C == '\n') { // new line
                     L++;          // keep track of line number for logging
                     P = 0;        // reset character position for logging
                 }
@@ -199,7 +206,7 @@ int main(int argc, char **argv)
                     fprintf(f.Log,"%lu [%lu]:  0x%x\n",L,P,C); // line_number: character_position C (in hex)
                 P++;                                    // keep track of character position for logging
             }
-			C = getc(f.In);
+            C = getc(f.In);
         }
         if (f.In  != stdin ) fclose(f.In);  // close infile
         if (f.Out != stdout) fclose(f.Out); // close outfile
